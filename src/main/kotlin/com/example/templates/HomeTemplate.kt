9168eae1df4b164a8c972e4410909358521e414f
alan.marcos.7e5 <alan.marcos.7e5@itb.cat>
2023-04-26 19:51:33 +0200

    Modified a bit the home view template

--- src/main/kotlin/com/example/templates/HomeTemplate.kt
+++ src/main/kotlin/com/example/templates/HomeTemplate.kt
@@ -9,15 +9,16 @@ class HomeTemplate(private val userSession: UserSession) : Template<FlowContent>
     override fun FlowContent.apply() {
         div("m-3") {
             div("d-flex justify-content-between mb-2") {
-                h4 { +"${userSession.name}" }
+                h4 { +"${userSession.name.uppercase()}" }
             }
-            div("d-flex justify-content-between mb-2") {
-                h4 { +"${AuthService.user}" }
+            div("d-flex flex-column justify-content-between mb-2") {
+                h5 { +"""Id: ${AuthService.user.id}""" }
+                h5 { +"""Rol: ${AuthService.user.role}""" }
             }
             p { +"""Has ingresado: ${userSession.count} veces""" }
             div {
                 a {
-                    href = "/logout"//TODO verify route and the log out
+                    href = "/logout"
                     button(classes="btn btn-secondary") {
                         +"""Log out"""
                     }
